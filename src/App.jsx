90ba573327133fb35507f4d70ae12277ca72218a
WIK2025 <https://github.com/WIK2025>
2026-06-07 23:51:33 +0700

    Загрузка проекта aviaTicket

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import TickedCard from './components/TickedCard/TickedCard'
+import Search from './components/Search/Search';
+import DataPicker from './components/DataPicker/DataPicker';
+import Loader from './components/Loader/Loader'; 
+import styles from './App.module.css';
+
 
 function App() {
-  const name = 'Александр Серов';
-  const role = 'Frontend Разработчик';
-  const description = 'Занимаюсь Изучением React и JS.';
+  const [tickets, setTickets] = useState([]); 
+  const [loading, setLodading] = useState(false);
+  const [origin, setOrigin] = useState('MOW');
+  const [destination, setDestination] = useState('LED');
+  const [departDate, setDeparteDate] = useState('');
+
+  useEffect(()=>{
+    const fetchData = async () => {
+    setLodading(true);  
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = `https://api.travelpayouts.com/v2/prices/latest?origin=${origin}&destination=${destination}&token=c4c4803704e456e75677a1714b230275`;
+    
+    try{
+    const response = await fetch(proxy+apiUrl);
+    if (response.ok){
+      const data = await response.json();
+      if (data.success) {
+        let allTickets = data.data;
+        if (departDate){
+          allTickets = allTickets.filter(ticket => ticket.depart_date === departDate);
+        }
+        setTickets(allTickets);
+      }
+    }} catch (error){
+      console.error('Ошибка загрузки данных:', error);
+    }finally {
+      setLodading(false);
+    }
+  };
+    
+    fetchData();
+  }, [origin, destination, departDate])
 
   return (
-    <div className="card-container">
-      <h1 style={{ 
-        color: '#2c3e50', 
-        marginBottom: '10px' 
-      }}>{name}</h1>
-      
-      {/* Подзаголовок */}
-      <h2 className="role-text">{role}</h2>
-      
-      {/* Параграф */}
-      <p className="description">
-        {description}
-      </p>
-      
-      {/* Смайлик  */}
-      <div className="emoji-section">
-        <span>&#128585;</span>
-        <blockquote>"Код — это все!!!"</blockquote>
+    <div className={styles.container}>
+      <h1>Поиск билетов</h1>
+      <div className={styles.searchBar}>
+        <Search 
+            placeholder='введите город отправления'
+            onSelect={setOrigin}
+        /> 
+        <Search 
+          placeholder='введите назначения'
+          onSelect={setDestination}
+        />
+        <DataPicker 
+          value={departDate}
+          onChange={setDeparteDate}
+        />
       </div>
+      {loading ? (
+        <Loader />
+      ) : (
+      <div className={styles.grid}>
+      {
+        tickets.map((ticket, index) => (
+        <TickedCard key={index} data={ticket} />
+        ))
+      }
+      </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App
+export default App;
