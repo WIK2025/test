98e54db7cf14ac6c1db81fb5016bd58f5114fa34
WIK2025 <https://github.com/WIK2025>
2026-08-24 20:43:06 +0700

    feat: add templates for post(base,list ,detail)

--- blog/post/urls.py
+++ blog/post/urls.py
@@ -5,5 +5,5 @@ from .import views
 app_name = 'post'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:post_id>/', views.post_details, name='post_detail'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'), 
 ]
