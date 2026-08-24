9aaf31cf5e7dc60b7fd35775280fc2eb446e5bbc
WIK2025 <https://github.com/WIK2025>
2026-08-24 19:51:04 +0700

    feat: configure urls for post app

---
+++ blog/post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .import views 
+
+# пространство имен: post:post_list
+app_name = 'post'
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/', views.post_details, name='post_detail'),
+]
