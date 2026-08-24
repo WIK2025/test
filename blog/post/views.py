e21211b9e5cede7a7d9e8bef36655ab5d87e3002
WIK2025 <https://github.com/WIK2025>
2026-08-24 19:41:52 +0700

    feat: add post_list and post_details views

--- blog/post/views.py
+++ blog/post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+# get_object_or_404 - получаем модель или ошибку 404
 # Create your views here.
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    context = {
+        'post': posts,# ключ к котррому будем обращаться в шаблоне
+        'page_title': 'Все посты блога'
+    }
+    return render(request, 'post/post_list.html', context)
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {
+        'post': post,
+        'page_title': post.title
+
+    }
+    return render(request, 'post/post_details.html', context)
+
