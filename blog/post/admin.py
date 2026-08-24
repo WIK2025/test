c6ecb4b25cec79c09cce2fbd4fad6d0402753d94
WIK2025 <https://github.com/WIK2025>
2026-08-24 19:26:32 +0700

    feat: register Post in admin panel

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'update_at')#добавляем кортеж
+    list_filter = ('created_at', 'update_at') #добавляем кортеж
+    search_fields = ('title', 'contant')# добавляем поисковую строка
+    readonly_fields = ('created_at', 'update_at')# поля только для чтения, заполнить нельзя
+    # группировка полей на странице редактирования
+    # каждый кортеж это отдельнв\ая группа
+    fieldsets = (
+        ('Основная информация',{
+        'fields': ('title', 'content')
+    }),
+    ('Даты', {
+        'fields': ('created_at', 'update_at'),
+        'classes': ('collapse') #сворачиваемая секция
+    }),
+    )
--- blog/post/models.py
+++ blog/post/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(
-        max_lenght=200,
+        max_length=200,
         help_text='Введите заголовок поста'
     )
     content = models.TextField(
