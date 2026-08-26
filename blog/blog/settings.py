7772bae119f63761a9c4c2cc6c79990dbef0dbd1
WIK2025 <https://github.com/WIK2025>
2026-08-26 20:32:34 +0700

    feat: add login/logout urls and setting

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Email
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),# будут доступны по корневому адресу
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'), # используем класс as_view() в качестве представления
+    path('logout/',  auth_views.LoginView.as_view(next_page='post:post_list', name='logout')),
+
 ]
 
