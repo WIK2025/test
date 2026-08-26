b2f0ee37d6afed1780dc11eda3e090d3a4c2a015
WIK2025 <https://github.com/WIK2025>
2026-08-26 21:16:09 +0700

    feat: add login, create/edit post templates

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = 'post:post_list'
+LOGOUT_REDIRECT_URL ='post:post_list'
 
 
 # Email
