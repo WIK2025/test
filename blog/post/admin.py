a20070cb9ef7d98de494d761454096f80c58ba79
WIK2025 <https://github.com/WIK2025>
2026-08-26 18:51:09 +0700

    feat: add author and Comment model in admin panel

--- blog/post/admin.py
+++ blog/post/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'update_at')#добавляем кортеж
-    list_filter = ('created_at', 'update_at') #добавляем кортеж
-    search_fields = ('title', 'contant')# добавляем поисковую строка
+    list_filter = ('created_at', 'update_at', 'author') #добавляем кортеж
+    search_fields = ('title', 'contant', 'author__username')# добавляем поисковую строка
     readonly_fields = ('created_at', 'update_at')# поля только для чтения, заполнить нельзя
     # группировка полей на странице редактирования
     # каждый кортеж это отдельнв\ая группа
@@ -18,3 +18,13 @@ class PostAdmin(admin.ModelAdmin):
         'classes': ('collapse') #сворачиваемая секция
     }),
     )
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('text__preview', 'post', 'author', 'created_at') # обрезка текста
+    list_filter = ('created_at', 'author')
+    search_fields = ('text', 'author__username', 'post__title')
+    def text_preview(self, obj):
+        return obj.text[:50] + '...' if len(obj.text)>50 else obj.text
+    text_preview.short_desctiption = 'Текст комментария'
+    
+
